72f4b1566d59eb51d6feefdeefb5ace89202f43f
Edward Wei <ewei2406@gmail.com>
2021-11-12 14:42:41 -0500

    more tests

--- CBQ/tests/test_account.py
+++ CBQ/tests/test_account.py
@@ -1,11 +1,11 @@
 from CBQ import account
 import os
 
-def test_account_creation():
-    SANDBOX_API_PASSPHRASE = os.environ['SANDBOX_API_PASSPHRASE']
-    SANDBOX_API_KEY = os.environ['SANDBOX_API_KEY']
-    SANDBOX_API_SECRET = os.environ['SANDBOX_API_SECRET']
+SANDBOX_API_PASSPHRASE = os.environ['SANDBOX_API_PASSPHRASE']
+SANDBOX_API_KEY = os.environ['SANDBOX_API_KEY']
+SANDBOX_API_SECRET = os.environ['SANDBOX_API_SECRET']
 
+def test_account_creation():
     acc = account.CoinbaseAccount(
         SANDBOX_API_KEY,
         SANDBOX_API_SECRET,
@@ -17,3 +17,19 @@ def test_account_creation():
     assert acc.API_SECRET == SANDBOX_API_SECRET
     assert acc.API_PASSPHRASE == SANDBOX_API_PASSPHRASE
     assert acc.BASE_PATH == "https://api-public.sandbox.exchange.coinbase.com"
+
+def test_get_wallets():
+    acc = account.CoinbaseAccount(
+        SANDBOX_API_KEY,
+        SANDBOX_API_SECRET,
+        SANDBOX_API_PASSPHRASE,
+        is_sandbox=True
+    )
+
+    portfolio = acc.get_wallets()
+    assert len(portfolio)
+    assert "amount" in portfolio[0]
+    assert "label" in portfolio[0]
+    assert "price" in portfolio[0]
+    assert "usd" in portfolio[0]
+    assert "percentage" in portfolio[0]
