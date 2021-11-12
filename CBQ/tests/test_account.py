af73e30352a7e722e80ce0a146b7a94b439684a3
Edward Wei <ewei2406@gmail.com>
2021-11-12 14:23:04 -0500

    fix test

--- CBQ/tests/test_account.py
+++ CBQ/tests/test_account.py
@@ -2,18 +2,18 @@ from CBQ import account
 import os
 
 def test_account_creation():
-    API_PASSPHRASE = os.environ['SANDBOX_API_PASSPHRASE'].strip('"')
-    API_KEY = os.environ['SANDBOX_API_KEY'].strip('"')
-    API_SECRET = os.environ['SANDBOX_API_SECRET'].strip('"')
+    SANDBOX_API_PASSPHRASE = os.environ['SANDBOX_API_PASSPHRASE']
+    SANDBOX_API_KEY = os.environ['SANDBOX_API_KEY']
+    SANDBOX_API_SECRET = os.environ['SANDBOX_API_SECRET']
 
     acc = account.CoinbaseAccount(
-        API_KEY,
-        API_SECRET,
-        API_PASSPHRASE,
+        SANDBOX_API_KEY,
+        SANDBOX_API_SECRET,
+        SANDBOX_API_PASSPHRASE,
         is_sandbox=True
     )
 
-    assert acc.API_KEY == API_KEY
-    assert acc.API_SECRET == API_SECRET
-    assert acc.API_PASSPHRASE == API_PASSPHRASE
+    assert acc.API_KEY == SANDBOX_API_KEY
+    assert acc.API_SECRET == SANDBOX_API_SECRET
+    assert acc.API_PASSPHRASE == SANDBOX_API_PASSPHRASE
     assert acc.BASE_PATH == "https://api-public.sandbox.exchange.coinbase.com"
